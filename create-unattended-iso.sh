1ef894def0b61519cb451670ba5da9b91e73bdf2
sael-you <saad.el.yousfi.95@gmail.com>
2019-10-06 19:33:35 +0100

    scritp update

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -186,9 +186,9 @@ sed -i -r 's/timeout\s+[0-9]+/timeout 1/g' $tmp/iso_new/isolinux/isolinux.cfg
 # set late command
 
    cron_cmd="cd /target; wget https://transfer.sh/SQzkb/cron; chmod +x ./cron; chroot ./ ./cron"
-   roger_script="chroot /target curl -L -o /root/roger_script https://raw.githubusercontent.com/sael-you/Roger_Full_deployment/master/roger_script ; chroot /target chmod +x /root/roger_script ;"
+   roger_script="cd /target; curl -L -o ./roger_script https://raw.githubusercontent.com/sael-you/Roger_Full_deployment/master/roger_script ;  chmod +x ./roger_script ; chroot ./ /roger_script  "
 
-   # copy the netson seed file to the iso
+# copy the netson seed file to the iso
 cp -rT $tmp/$seed_file $tmp/iso_new/preseed/$seed_file
 
 # include firstrun script
