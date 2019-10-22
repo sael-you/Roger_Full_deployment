50275e53cd86454c51f2245a6f95a7babefdb7a1
sael-you <saad.el.yousfi.95@gmail.com>
2019-10-22 12:55:24 +0100

    roger OK ALL SET ALL GOOD

--- unattended_iso_creation/create-unattended-iso.sh
+++ unattended_iso_creation/create-unattended-iso.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # file names & paths
-tmp="."  # destination folder to store the final iso file
+tmp="$HOME"  # destination folder to store the final iso file
 hostname="ubuntu"
 currentuser="$( whoami)"
 
